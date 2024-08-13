9a6e87a4a015176b7994f0dd9252cc8eb2ae5159
ZanderKeith <zkeith@mit.edu>
2024-08-13 14:11:18 -0400

    /home/zkeith/miniconda3/envs/barc-blanket-env/bin/python

--- run_all_cases_postprocess.py
+++ run_all_cases_postprocess.py
@@ -7,10 +7,11 @@ from barc_blanket.models.barc_model_final import BLANKET_MATERIAL_ID
 for case, config in CASES.items():
     try:
         with working_directory(f"depletion_results/{case}"):
+            print(case)
             postprocess_coupled_depletion(BLANKET_MATERIAL_ID, remove_C14=False)
             plot_results(case, config['name'])
-            postprocess_coupled_depletion(BLANKET_MATERIAL_ID, remove_C14=True)
-            plot_results(f"{case}_no_C14", f"{config['name']} (no C14)")
-    except:
-        pass
-
+            # postprocess_coupled_depletion(BLANKET_MATERIAL_ID, remove_C14=True)
+            # plot_results(f"{case}_no_C14", f"{config['name']} (no C14)")
+    except Exception as e:
+        print(f"Error in case {case}: {e}")
+        continue
